504a10349b8e3e45747bcd4bce3c653c0fac9a32
Rohit Thakre <rsthakre@gmail.com>
2019-01-24 20:43:03 -0500

    Part2
    Moving the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    //Called before rendering a frame
+    void Update()
+    {
+
+    }
+
+    //Called before making any physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement);
+
+    }
+
+}
